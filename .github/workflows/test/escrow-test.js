2da1f0bb531988a1e977d0a511c8e740b70587c4
root <root@ip-172-31-52-21.us-west-2.compute.internal>
2021-10-28 18:41:29 +0000

    Made tests as expected

--- .github/workflows/test/escrow-test.js
+++ .github/workflows/test/escrow-test.js
@@ -1,42 +1,68 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const {loadFixture, deployContract} = require('ethereum-waffle');
+
+//console.log('Hello')
 
 describe("Escrow", async function () {
-  it("Do everything", async function () {
+
+  async function fixture([], provider) {
     const Escrow = await hre.ethers.getContractFactory("Escrow");
     const escrow = await Escrow.deploy();
-
-    await escrow.deployed();
-
     const [payer, payee, releaser, acc1] = await ethers.getSigners();
+    await escrow.deployed();
+    return {escrow, payer, payee, releaser, acc1};
+  }
 
+  it('Deposit', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
     expect(
      await escrow.connect(payer).depositFor(payee.address,releaser.address,{
           value: 100
       })
-    ).to.changeEtherBalances([payer, escrow], [-100, 100]);      
-   
+    ).to.changeEtherBalances([payer, escrow], [-100, 100]);
+  });
+
+  /*
+
+  it('Using wrong account to release', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
     expect(
      await escrow.connect(acc1).release(payer.address,payee.address)
     ).to.changeEtherBalances([escrow, payee], [-100, 100]);  
+  });
 
-    expect(
-     await escrow.connect(releaser).release(acc1.address,payee.address)
-    ).to.changeEtherBalances([escrow, payee], [-100, 100]);
- 
+  it('Using wrong payee to release', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
     expect(
      await escrow.connect(releaser).release(payer.address,acc1.address)
     ).to.changeEtherBalances([escrow, payee], [-100, 100]);
+  });
 
+  */
+
+  it('Release normally', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
     expect(
      await escrow.connect(releaser).release(payer.address,payee.address)
     ).to.changeEtherBalances([escrow, payee], [-100, 100]);
+  });
 
+  it('Deposit again', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
+    expect(
+     await escrow.connect(payer).depositFor(payee.address,releaser.address,{
+          value: 100
+      })
+    ).to.changeEtherBalances([payer, escrow], [-100, 100]);
+  });  
+
+  it('Refund normally', async () => {    
+    const {escrow, payer, payee, releaser, acc1} = await loadFixture(fixture);    
     expect(
      await escrow.connect(releaser).refund(payer.address,payee.address)
     ).to.changeEtherBalances([escrow, payer], [-100, 100]);
-    
-  });          
+  });     
 
 
 });
